1f21860a758f0d97ac2704a306419aedd5debcbc
Leo Cheung <leozhang621@gmail.com>
2024-04-11 15:28:21 +0800

    add question text

--- src/questions/N83_DeleteDuplicatesLinkedList.java
+++ src/questions/N83_DeleteDuplicatesLinkedList.java
@@ -2,6 +2,20 @@ package questions;
 
 import utils.ListNode;
 
+/**
+ * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+ * <p>
+ * 示例 1：
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ * <p>
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ * <p>
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序 排列
+ */
 class SolutionN83 {
     public ListNode deleteDuplicates(ListNode head) {
         if(head == null || head.next == null) {
