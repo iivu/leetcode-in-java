ea005a60a7f3c2b2dd436f54f1c8b2725fd1e411
Leo Cheung <leozhang621@gmail.com>
2024-04-12 10:47:17 +0800

    add N104 question comment

--- src/questions/N104_MaxDepth.java
+++ src/questions/N104_MaxDepth.java
@@ -2,6 +2,12 @@ package questions;
 
 import utils.TreeNode;
 
+/**
+ * 给定一个二叉树 root ，返回其最大深度。
+ * <p>
+ * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+ */
+
 // 分解子问题，左节点的深度和右节点深度最大的一个，加上自己
 class SolutionN104_1 {
     public int maxDepth(TreeNode root) {
