27368cde32c4e91fbbdf56eefab8ab8326c008ee
Leo Cheung <leozhang621@gmail.com>
2024-04-16 11:16:07 +0800

    add N111

---
+++ src/questions/N111_MinDepth.java
@@ -0,0 +1,39 @@
+package questions;
+
+import utils.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 给定一个二叉树，找出其最小深度。
+ * <p>
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ */
+class SolutionN111 {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int ans = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                if (curr.left == null && curr.right == null) {
+                    return ans;
+                }
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+            ans++;
+        }
+        return ans;
+    }
+}
