8b48a4cfbccfd3e117a908905bb4234ac3b51dd0
Leo Cheung <leozhang621@gmail.com>
2024-04-18 10:40:02 +0800

    add N62

--- src/questions/N62_UniquePaths.java
+++ src/questions/N62_UniquePaths.java
@@ -1,39 +1,51 @@
 package questions;
 
+import java.util.Arrays;
+
+/**
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * <p>
+ * 问总共有多少条不同的路径？
+ */
 class SolutionN62 {
     private int ans = 0;
-    private final int[] curr = new int[]{1, 1};
 
+    // TODO: 超时
     public int uniquePaths(int m, int n) {
-        backtrack(m, n);
+        // memo记录已经走过的格子
+        int[][] memo = new int[m + 1][n + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        backtrack(1, 1, m, n, memo);
         return ans;
     }
 
-    private void backtrack(int m, int n) {
-        if (curr[0] == m && curr[1] == n) {
+    private int backtrack(int cm, int cn, int m, int n, int[][] memo) {
+        if (cm == m && cn == n) {
             ans++;
-            return;
+            return 1;
         }
-        for (int i = 0; i < 2; i++) {
-            // 0向右移动1向下移动
-            if (i == 0 && canMoveRight(n)) {
-                curr[1]++;
-                backtrack(m, n);
-                curr[1]--;
-            }
-            if (i == 1 && canMoveBottom(m)) {
-                curr[0]++;
-                backtrack(m, n);
-                curr[0]--;
-            }
+        if (memo[cm][cn] != -1) {
+            ans++;
+            return 1;
         }
+        if (canMoveRight(cn, n)) {
+           backtrack(cm, cn + 1, m, n, memo);
+        }
+        if (canMoveBottom(cm, m)) {
+            return memo[cm][cn] = backtrack(cm + 1, cn, m, n, memo);
+        }
+        return -1;
     }
 
-    private boolean canMoveRight(int n) {
-        return curr[1] < n;
+    private boolean canMoveRight(int cn, int n) {
+        return cn < n;
     }
 
-    private boolean canMoveBottom(int m) {
-        return curr[0] < m;
+    private boolean canMoveBottom(int cm, int m) {
+        return cm < m;
     }
 }
