5afdf7b22454fd56eb7b017b5e32a228aefb7809
Leo Cheung <leozhang621@gmail.com>
2024-05-07 10:21:08 +0800

    add N88

--- src/questions/N88_Merge.java
+++ src/questions/N88_Merge.java
@@ -1,2 +1,30 @@
 package questions;
 
+/**
+ * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+ * <p>
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ * <p>
+ * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+ */
+class SolutionN88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1;
+        int p = nums1.length - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] >=  nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+        while (p2 >=0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
+    }
+}
