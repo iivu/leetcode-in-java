8a934b4c98c7c95753b8893af1e129f2a31236ce
Leo Cheung <leozhang621@gmail.com>
2024-05-08 18:06:07 +0800

    optimize N167

--- src/questions/N167_TwoSum2.java
+++ src/questions/N167_TwoSum2.java
@@ -12,9 +12,9 @@ package questions;
 class SolutionN167 {
 
     public int[] twoSum(int[] numbers, int target) {
-        int left = 0, right = numbers.length - 1, sum = 0;
+        int left = 0, right = numbers.length - 1;
         while (left < right) {
-            sum = numbers[left] + numbers[right];
+            int sum = numbers[left] + numbers[right];
             if (sum == target) {
                 return new int[]{left + 1, right + 1};
             } else if (sum > target) {
