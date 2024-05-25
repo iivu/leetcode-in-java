0473b4c3073fea5a1ca57fbc983fbe455260e17d
Leo Cheung <leozhang621@gmail.com>
2024-05-25 16:12:01 +0800

    add N34

---
+++ src/questions/N34_SearchRange.java
@@ -0,0 +1,55 @@
+package questions;
+
+/**
+ * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+ * <p>
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ * <p>
+ * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+ */
+class SolutionN34 {
+    public int[] searchRange(int[] nums, int target) {
+        int left = searchLeft(nums, target);
+        if (left == -1) {
+            return new int[]{-1, -1};
+        } else {
+            return new int[]{left, searchRight(nums, target)};
+        }
+    }
+
+    private int searchLeft(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid - 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        if (left < 0 || left >= nums.length) {
+            return -1;
+        }
+        return nums[left] == target ? left : -1;
+    }
+
+    private int searchRight(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        if (right < 0 || right >= nums.length) {
+            return -1;
+        }
+        return nums[right] == target ? right : -1;
+    }
+}
