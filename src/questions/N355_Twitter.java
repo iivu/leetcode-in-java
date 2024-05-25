76b9d5ec346c638d133b2484dc4650ff27ce952e
Leo Cheung <leozhang621@gmail.com>
2024-05-25 11:11:26 +0800

    perfect N355

--- src/questions/N355_Twitter.java
+++ src/questions/N355_Twitter.java
@@ -38,18 +38,6 @@ import java.util.stream.Collectors;
  * postTweet、getNewsFeed、follow 和 unfollow 方法最多调用 3 * 104 次
  */
 class Twitter {
-
-    public static void main(String[] args) {
-        Twitter twitter = new Twitter();
-        twitter.postTweet(1, 5); // 用户 1 发送了一条新推文 (用户 id = 1, 推文 id = 5)
-        twitter.getNewsFeed(1);  // 用户 1 的获取推文应当返回一个列表，其中包含一个 id 为 5 的推文
-        twitter.follow(1, 2);    // 用户 1 关注了用户 2
-        twitter.postTweet(2, 6); // 用户 2 发送了一个新推文 (推文 id = 6)
-        twitter.getNewsFeed(1);  // 用户 1 的获取推文应当返回一个列表，其中包含两个推文，id 分别为 -> [6, 5] 。推文 id 6 应当在推文 id 5 之前，因为它是在 5 之后发送的
-        twitter.unfollow(1, 2);  // 用户 1 取消关注了用户 2
-        twitter.getNewsFeed(1);  // 用户 1 获取推文应当返回一个列表，其中包含一个 id 为 5 的推文。因为用户 1 已经不再关注用户 2
-    }
-
     private static int time = 1;
     private static int PER_PERSON_FETCH_TWEET_MAX_COUNT = 10;
 
@@ -78,8 +66,7 @@ class Twitter {
         if (!timelines.containsKey(userId)) {
             timelines.put(userId, new Tweet(tweetId, userId));
         } else {
-            Tweet last = timelines.get(userId);
-            timelines.put(userId, new Tweet(tweetId, userId, last));
+            timelines.compute(userId, (k, last) -> new Tweet(tweetId, userId, last));
         }
     }
 
