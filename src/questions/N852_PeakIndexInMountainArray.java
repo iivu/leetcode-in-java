cbfc72ddc93127f56404e59b31d7a108492034f7
Leo Cheung <leozhang621@gmail.com>
2024-05-27 10:48:24 +0800

    add N852

---
+++ src/questions/N852_PeakIndexInMountainArray.java
@@ -0,0 +1,16 @@
+package questions;
+
+class SolutionN852 {
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                r = mid;
+            }else if (arr[mid] < arr[mid + 1]) {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
