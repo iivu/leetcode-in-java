3b6f122d4e0697ec9e1e4e8d0eeae08850b2ada8
Leo Cheung <leozhang621@gmail.com>
2024-05-30 14:46:28 +0800

    add N1038

---
+++ src/questions/N1038_BSTToGST.java
@@ -0,0 +1,24 @@
+package questions;
+
+import utils.TreeNode;
+
+/**
+ * 给定一个二叉搜索树 root (BST)，请将它的每个节点的值替换成树中大于或者等于该节点值的所有节点值之和。
+ */
+class SolutionN1038 {
+    private int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    private void traverse(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        traverse(node.right);
+        sum += node.val;
+        node.val = sum;
+        traverse(node.left);
+    }
+}
