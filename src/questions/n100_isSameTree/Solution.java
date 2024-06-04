f23a24a9316830feacbe8256a7f9c32c11bb5c4b
Leo Cheung <leozhang621@gmail.com>
2024-06-04 10:55:28 +0800

    add N100

---
+++ src/questions/n100_isSameTree/Solution.java
@@ -0,0 +1,23 @@
+package questions.n100_isSameTree;
+
+import utils.TreeNode;
+
+/**
+ * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ * <p>
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
